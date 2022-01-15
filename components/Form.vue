76434d327b6ac6319e65c42bd6c95e118c58acc7
Idris Olubisi <heedris2olubisi@gmail.com>
2022-01-15 23:11:33 +0100

    File uploaded

---
+++ components/Form.vue
@@ -0,0 +1,58 @@
+<template>
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+        Name
+      </label>
+      <input
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="name"
+        type="text"
+        placeholder="name"
+        ref="name"
+      />
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="feedback">
+        Feedback
+      </label>
+      <input
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="feedback"
+        type="text"
+        placeholder="feedback"
+        ref="feedback"
+      />
+    </div>
+    <div class="flex items-center justify-between">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        @click.prevent="getFormValues()"
+      >
+        Submit
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      feedback: "",
+      videoEnded: "",
+    };
+  },
+  methods: {
+    getFormValues() {
+      alert(`Feedback received successfully!`);
+      console.log(this.$refs.name.value);
+      console.log(this.$refs.feedback.value);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex justify-center items-center h-screen space-x-10">
+    <div class="rounded overflow-hidden shadow-lg mb-4 content-center">
+      <div class="px-6 py-4 pb-2">
+        <div class="font-bold text-xl mb-2">Video Event Trigger - Nuxt</div>
+        <video
+          id="video-player"
+          controls
+          autoplay
+          muted
+          width="500px"
+          class="width-full"
+        ></video>
+      </div>
+    </div>
+
+    <Form v-if="ended"></Form>
+  </div>
+</template>
+
+<script>
+import Form from "../components/Form.vue";
+export default {
+  data() {
+    return {
+      cld: null,
+      player: null,
+      video: "samples/animals/testvid",
+      ended: null,
+    };
+  },
+
+  mounted() {
+    this.cld = cloudinary.Cloudinary.new({
+      cloud_name: process.env.NUXT_ENV_CLOUDINARY_CLOUD_NAME,
+      secure: true,
+    });
+
+    this.player = this.cld.videoPlayer("video-player", {
+      playedEventTimes: [10, 20, 30, 40, 50, 60, 70, 80, 90],
+      analytics: {
+        events: ["ended"],
+      },
+    });
+
+    this.player.source(this.video);
+
+    this.player.on("ended", () => {
+      this.ended = true;
+    });
+  },
+  components: {
+    Form,
+  },
+  name: "IndexPage",
+};
+</script>
